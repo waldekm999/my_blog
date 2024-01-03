821767e8e0e10c78f9cd2fc6c377d3b60657d26d
waldekm999 <waldekm999@hotmail.com>
2024-01-03 21:18:21 +0100

    views posts and posts also urls blog added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+# create function which will return a list of all the posts
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+# create function which will return a single post
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
